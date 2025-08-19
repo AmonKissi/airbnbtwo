dae9b19f5be2cc10b1f42937dabf609fb4d83247
ComputaGeek <81460315+AmonKissi@users.noreply.github.com>
2025-08-19 02:33:07 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`AirBnb One - ${siteConfig.title}`}
+      title={`AirBnb Two - ${siteConfig.title}`}
       description="Guide">
       <HomepageHeader />
       <main>
